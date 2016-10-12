d1c9c46f247fb73e035be62dcf511cd8bbac0189
Dongshan <thomasyang1206@126.com>
2016-10-12 19:46:01 +0800

    add nrf2401 functions

--- nrf2401/nrf2401.c
+++ nrf2401/nrf2401.c
@@ -8,9 +8,15 @@
 #include "spi.h"
 
 
-#define digitalWrite(pin, value) bcm2835_gpio_write(pin, value)
-#define pinMode(pin,value) bcm2835_gpio_fsel(pin,value);
+#define nrf24_max(a,b) (a>b?a:b)
+#define nrf24_min(a,b) (a<b?a:b)
+
+
+#define digital_write(pin, value) bcm2835_gpio_write(pin, value)
+#define pin_mode(pin,value) bcm2835_gpio_fsel(pin,value);
 #define OUTPUT BCM2835_GPIO_FSEL_OUTP
+
+#define BV(n) (1 << n)
 
 struct nrf24 {
 	uint8_t ce_pin;
@@ -19,22 +25,51 @@ struct nrf24 {
 	uint8_t spi_rxbuf[32 + 1];
 	uint8_t spi_tx_buf[32 + 1];
 	uint8_t addr_width;
-	uint8_t rx1_addr[5];
-	uint8_t tx1_addr[5];
+	uint8_t *rx1_addr;
+	uint8_t *tx1_addr;
+    uint8_t payload_size;
+    uint32_t tx_rx_delay;
 };
+
+struct nrf24 rf24;
 
 
 void set_csn_pin_level(uint8_t pin, bool level)
 {
-	digitalWrite(pin, level);
+	digital_write(pin, level);
 	//delayMicroseconds(5);
 	usleep(5);
 }
 
 void set_ce_pin_level(uint8_t pin, bool level)
 {
-	digitalWrite(pin, level);
+	digital_write(pin, level);
 }
+
+uint8_t nrf24_flush_rx(void)
+{
+    uint8_t status;
+
+    nrf24_begin_transaction();
+    status = spi_transfer(FLUSH_RX);
+    nrf24_end_transaction();
+
+    return status;
+}
+
+uint8_t nrf24_flush_tx(void)
+{
+    uint8_t status;
+
+    nrf24_begin_transaction();
+
+    status = spi_transfer(FLUSH_TX);
+
+    nrf24_end_transaction();
+
+    return status;
+}
+
 
 void nrf24_begin_transaction()
 {
@@ -48,7 +83,7 @@ void nrf24_end_transaction(void)
 	set_csn_pin_level(csn_pin, HIGH);
 }
 
-uint8_t nrf24_read_register(uint8_t reg, uint8_t *buf, uint8_t len)
+uint8_t nrf24_read_register(uint8_t reg)
 {
 	uint8_t status;
 
@@ -76,6 +111,175 @@ uint8_t nrf24_write_register(uint8_t reg, uint8_t val)
 	nrf24_end_transaction();
 
 	return status;
+}
+
+u
+
+int8_t nrf24_wirte_payload(struct nrf24 *rf24, const void *buf, uint8_t len, uint8_t type)
+{
+    uint8_t status;
+    uint8_t *p = buf;
+
+    len = rf24_min(len, rf24->payload_size);
+    uint8_t blank_len = rf24->payload_size - len;
+    
+	nrf24_begin_transaction();
+
+    status = spi_transfer(type);
+
+    while (len--) {
+        spi_transfer(*p++);
+    }
+
+    while (blank_len--) {
+        spi_transfer(0);
+    }
+
+    nrf24_end_transaction();
+}
+
+uint8_t nrf24_read_payload(struct nrf24 *nrf24, const uint8_t buf, uint8_t len)
+{
+    uint8_t status;
+    uint8_t *p = buf;
+
+    len = rf24_min(len, rf24->payload_size);
+
+    nrf24_begin_transaction();
+
+    status = spi_transfer(R_RX_PAYLOAD);
+    while (len--) {
+        *p++ = spi_transfer(0xff);
+    }
+    
+    nrf24_end_transaction();
+
+    return status;
+}
+
+
+void nrf24_set_channel(uint8_t channel)
+{
+    const uint8_t max_channel = 125;
+
+    nrf24_write_register(RF_CH, nrf24_min(channel, max_channel));
+}
+
+void nrf24_get_channel(void)
+{
+    return nrf24_read_register(RF_CH);
+}
+
+void nrf24_set_payload_size(struct nrf24 *nrf24, uint8_t size)
+{
+    nrf24->payload_size = size;
+}
+
+uint8_t nrf24_set_payload_size(struct nrf24 *nrf24)
+{
+    return nrf24->payload_size;
+}
+
+void nrf24_disable_crc(void)
+{
+    uint8_t disable = nrf24_read_register(NRF_CONFIG) & ~BV(EN_CRC);
+    nrf24_write_register(NRF_CONFIG, disable);
+}
+
+void nrf24_set_retries(uint8_t delay, uint8_t count)
+{
+    nrf24_write_register(SETUP_RETR, (delay&0xf)<<ARD | (count&0xf)<<ARC)
+}
+
+bool nrf24_set_data_rate(struct nrf24 *nrf24, rf24_datarate_e speed)
+{
+    bool result = false;
+    uint8_t setup = nrf24_read_register(RF_SETUP);
+
+    setup &= ~(BV(RF_DR_LOW) | BV(RF_DR_HIGH));
+
+    switch(speed) {
+        default:
+        case RF24_250KBPS: 
+            {
+            setup |= BV(RF_DR_LOW);
+            nrf24->tx_rx_delay = 450;
+            break;
+        }
+        case RF24_1MBPS:
+        case RF24_2MBPS: {
+            setup |= BV(RF_DR_HIGH);
+            nrf24->tx_rx_delay = 190;
+            break;
+        }
+    }
+
+    nrf24_write_register(RF_SETUP, setup);
+
+    if (nrf24_read_register(RF_SETUP) == setup) {
+        result = true;
+    }
+
+    return result;
+    
+}
+
+void nrf24_power_up(void)
+{
+    uint8_t cfg = nrf24_read_register(NRF_CONFIG);
+
+    nrf24_write_register(NRF_CONFIG, cfg | BV(PWR_UP));
+    delay(5);
+}
+
+uint8_t nrf24_init(const uint8_t ce_pin, const uint8_t csn_pin)
+{
+    struct nrf24 *nrf = &nrf24;
+    static const uint8_t rx_addr[] = {0x00, 0x01, 0x02, 0x03, 0x04};
+    static const uint8_t tx_addr[] = {0x00, 0x01, 0x02, 0x03, 0x04}
+
+    memset(nrf, 0, sizeof(struct nrf24));
+    nrf->ce_pin = ce_pin;
+    nrf->csn_pin = csn_pin;
+    nrf->rx1_addr = rx_addr;
+    nrf->tx1_addr = tx_addr;
+
+    spi_begin();
+    pinmode(nrf->ce_pin,OUTPUT);
+    set_csn_pin_level(nrf->ce_pin, LOW);
+    delay(100);
+    
+    // reset NRF_CONFIG and enable 16 bit CRC
+    nrf24_write_register(NRF_CONFIG, 0b00001100);
+
+    // set retries
+    nrf24_set_retries(5, 15);
+
+    if (false == nrf24_set_data_rate(nrf24,RF24_250KBPS))
+    {
+        // set data rate fail!!!
+    }
+
+    nrf24_set_data_rate(nrf24, RF24_1MBPS);
+    // TODO: ommit toggle feature compared with RF24
+    nrf24_write_register(FEATURE, 0);
+    nrf24_write_register(DYNPD, 0);
+    // Reset current status
+    uint8_t status = BV(RX_DR) | BV(TX_DS) | BV(MAX_RT); 
+    nrf24_write_register(NRF_STATUS, status);
+
+    // TODO: why it is 76
+    nrf24_set_channel(76);
+
+    nrf24_flush_rx();
+    nrf24_flush_tx();
+
+    nrf24_power_up();
+
+    nrf24_write_register(NRF_CONFIG, read_register(NRF_CONFIG) & ~BV(PRIM_RX));
+
+    return (setup != 0 && setup != 0xff)
+    
 }
 
 
